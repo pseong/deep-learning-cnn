172b50d3e741fbcefe831bcd1c927e5a7c2e740d
pseong <pkseonguk@gmail.com>
2022-01-25 18:09:30 +0900

    Add commnets in trainer.py

--- common/trainer.py
+++ common/trainer.py
@@ -17,7 +17,7 @@ class Trainer:
         mini_batch_size=100,    # 학습 시 배치 크기
         optimizer='SGD',        # 가중치와 편향을 조정하는 방법
         optimizer_param={'lr':0.01},        # 학습률
-        evaluate_sample_num_per_epoch=None,
+        evaluate_sample_num_per_epoch=None, # 1에폭당 평가할 데이터 수
         verbose=True # 로깅 여부
     ):
         self.network = network
@@ -53,32 +53,42 @@ class Trainer:
         self.test_acc_list = []
 
     def train_step(self):
-        batch_mask = np.random.choice(self.train_size, self.batch_size)
-        x_batch = self.x_train[batch_mask]
-        t_batch = self.t_train[batch_mask]
+        batch_mask = np.random.choice(self.train_size, self.batch_size) # 배치 크기만큼 전체 데이터셋에서 랜덤으로 선택
+        x_batch = self.x_train[batch_mask] # 학습용 이미지에서 선택된 값만 추출
+        t_batch = self.t_train[batch_mask] # 정답 레이블에서 선택된 값만 추출
         
-        grads = self.network.gradient(x_batch, t_batch)
-        self.optimizer.update(self.network.params, grads)
+        grads = self.network.gradient(x_batch, t_batch) # 네트워크의 가중치(W)와 편향(b)의 기울기 계산
+        self.optimizer.update(self.network.params, grads) # 네트워크의 가중치(W)와 편향(b) 갱신
         
-        loss = self.network.loss(x_batch, t_batch)
-        self.train_loss_list.append(loss)
-        if self.verbose: print("train loss:" + str(loss))
+        loss = self.network.loss(x_batch, t_batch) # 네트워크의 손실 계산 (학습이 잘 되어있는지 확인하기 위해 로그 남김)
+        self.train_loss_list.append(loss) # 손실 저장
+        if self.verbose: print("train loss:" + str(loss)) # 손실 출력
         
+        # 1에폭 당 학습 데이터 정확도와 테스트 데이터 정확도 계산
         if self.current_iter % self.iter_per_epoch == 0:
             self.current_epoch += 1
             
             x_train_sample, t_train_sample = self.x_train, self.t_train
             x_test_sample, t_test_sample = self.x_test, self.t_test
+
+            # 1에폭당 평가할 데이터 수가 정해져 있지 않다면 모든 데이터로 정확도 측정
+            # 1에폭당 평가할 데이터 수가 정해져 있다면 원하는 개수로 정확도 측정
             if not self.evaluate_sample_num_per_epoch is None:
                 t = self.evaluate_sample_num_per_epoch
+
+                # 정확도를 계산할 학습용 데이터와 테스트용 데이터를 원하는 개수만큼 짜름
                 x_train_sample, t_train_sample = self.x_train[:t], self.t_train[:t]
                 x_test_sample, t_test_sample = self.x_test[:t], self.t_test[:t]
-                
+
+            # 정확도 측정
             train_acc = self.network.accuracy(x_train_sample, t_train_sample)
             test_acc = self.network.accuracy(x_test_sample, t_test_sample)
+
+            # 측정한 정확도 저장
             self.train_acc_list.append(train_acc)
             self.test_acc_list.append(test_acc)
 
+            # 로그 출력
             if self.verbose: print("=== epoch:" + str(self.current_epoch) + ", train acc:" + str(train_acc) + ", test acc:" + str(test_acc) + " ===")
         self.current_iter += 1
 
