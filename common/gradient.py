d5e1863526409e5626b694ae74324f98c0a61c97
pseong <pkseonguk@gmail.com>
2022-01-26 03:12:50 +0900

    Add commnets in gradients.py

--- common/gradient.py
+++ common/gradient.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 import numpy as np
 
+# 기울기를 구할 때 수치적으로 기울기를 구하는 방식
+# 속도가 굉장히 느림
+# 해석적 기울기 구하는 방식이 제대로 작동하는지 테스트 하기 위해서 쓰임
+# 이 모델에서는 사용 안됨
+
 def _numerical_gradient_1d(f, x):
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
